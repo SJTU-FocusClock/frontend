9ced137661f9306dae08f904ba22efc11461f3fd
Whisey Jiang <Whisey@sjtu.edu.cn>
2020-10-04 17:30:54 +0800

    feat:新增闹钟的列表与新增的简单页面

---
+++ pages/clocklist/quickclock.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<uni-list>
+			<uni-list-item title="选择闹钟游戏" showArrow="true">
+				
+			</uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
