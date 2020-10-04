7685cdc559ce1b588bc8ac15a98978f00c37afee
Whisey <54048793+Whisey@users.noreply.github.com>
2020-10-04 17:34:16 +0800

    feat：新增闹钟列表与新增的简单页面

--- pages/clocklist/clocklist.vue
+++ pages/clocklist/clocklist.vue
@@ -6,7 +6,6 @@
 				<view style="width: 250px;height: 150px;padding-left: 75px;padding-top: 50px;">
 					<image style="width: 100px; height: 100px; border-radius:100px;" :mode="'scaleToFill'" src="../../static/avatar.png" clickable @click="onClick"></image>
 				</view>
-
 				<uni-list :border="false">
 					<uni-list-item :border="false" :show-extra-icon="true" :extra-icon="chatboxes" clickable title="Friends"  @click="onClick1" >
 					</uni-list-item>
@@ -16,9 +15,40 @@
 						
 					</uni-list-item>
 				</uni-list>
-
+				
 			</uni-drawer>
 		</view>
+		
+		<uni-list class="clockList">
+			<uni-list-item class="clockListItem" v-for="item in clockListData"
+				:clickable="true" @click="onClickClock" 
+				:title="item.hour+':'+item.minute" :note="item.tag">
+				<template v-slot:header>
+					<text v-if="item.hour < 12">上午</text>
+					<text v-else font-size="small">下午</text>
+				</template>
+				<template v-slot:footer>
+					<image src="../../static/avatar.png" class="clock-img"></image>
+				</template>
+			</uni-list-item>
+		</uni-list>
+		
+		<!-- <view class="clockList">
+			<view class="clockListItem" v-for="item in clockListData">
+				<view class="VMAM">
+					<text v-if="item.hour < 12">上午</text>
+					<text v-else>下午</text>	
+				</view>
+				<view class="timeAndDay">
+					<text class="time">{{item.hour}}:{{item.minute}}</text>
+					<text class="day">{{item.week}}</text>
+				</view>
+			</view>
+			
+			
+		</view> -->
+		<uni-fab ref="fab" :pattern="pattern" :content="content" horizontal="left" vertical="bottom" direction="horizontal" @trigger="trigger"/>
+		
 	</view>
 </template>
 
@@ -26,11 +56,13 @@
 	import uniDrawer from "@/components/uni-drawer/uni-drawer.vue"
 	import uniNavBar from "@/components/uni-nav-bar/uni-nav-bar.vue"
 	import uniIcons from "@/components/uni-icons/uni-icons.vue"
+	//import DateTimePicker from "@/components/bory-dateTimePicker/bory-dateTimePicker.vue"
+
 	export default {
 		components: {
 			uniDrawer,
 			uniNavBar,
-			uniIcons
+			uniIcons,
 		},
 		data() {
 			return {
@@ -49,7 +81,54 @@
 					color: '#93989d',
 					size: '22',
 					type: 'gear'
-				}
+				},
+				clockListData: [
+					{
+						id: 1,//闹钟id
+						hour: 6,//小时
+						minute: 30,//分钟
+						state: 1,//闹钟状态，开1，关0
+						tag: "起床", //备注
+						ring: "tmp", //铃声
+						isRing: 1, //是否响铃，开1，关0
+						type: 1,//重复类型，1为每周重复，2为特定日期闹钟
+						week: "11",//一周重复星期的参数
+						date: "",//闹钟指定日期
+					},
+					{
+						id: 2,//闹钟id
+						hour: 14,//小时
+						minute: 30,//分钟
+						state: 1,//闹钟状态，开1，关0
+						tag: "起床", //备注
+						ring: "tmp", //铃声
+						isShake: 1, //是否振动，开1，关0
+						isRing: 1, //是否响铃，开1，关0
+						type: 1,//重复类型，1为每周重复，2为特定日期闹钟
+						week: "11",//一周重复星期的参数
+						date: "",//闹钟指定日期
+					},
+				],
+				pattern: {
+					color: '#7A7E83',
+					backgroundColor: '#fff',
+					selectedColor: '#c4c4e9',
+					buttonColor: '#c4c4e9'
+				},
+				content: [
+					{
+						iconPath: '/static/clock2.png',
+						selectedIconPath: '/static/clock2.png',
+						text: '闹钟',
+						active: false
+					},
+					{
+						iconPath: '/static/quickclock.png',
+						selectedIconPath: '/static/quickclock.png',
+						text: '快速闹钟',
+						active: false
+					}
+				]
 			}
 		},
 		methods: {
@@ -57,8 +136,8 @@
 			onClick(e){
 				console.log('执行click事件', e.data)
 				uni.navigateTo({
-				                url: '/pages/userinfo/userinfo',
-				            });	
+					url: '/pages/userinfo/userinfo',
+				});	
 			},
 			onClick1(e) {
 				console.log('执行click事件', e.data)
@@ -77,6 +156,26 @@
 				uni.navigateTo({
 				                url: '/pages/set/set',
 				            });
+			},
+			onClickClock(e) {
+				console.log("点击闹钟进入编辑页面", e.data)
+				//if()
+				// uni.navigateTo({
+					
+				// })
+			},
+			trigger(e) {
+				console.log(e)
+				if(e.index == 0) {
+					uni.navigateTo({
+						url:'/pages/clocklist/normalclock?id=0',
+					});
+				}
+				if(e.index == 1) {
+					uni.navigateTo({
+						url:'/pages/clocklist/quickclock',
+					});
+				}
 			},
 			// 打开窗口
 			showDrawer(e) {
@@ -111,4 +210,17 @@
 		align-items: center;
 		justify-content: center;
 	}
+	
+	.clock-img {
+		height: auto;
+		width: 20%;
+	}
+	
+	.uni-list
+	.uni-list-item__content-title {
+		font-size:xx-large;
+		margin-left: 10px;
+	}
+	
+	
 </style>
---
+++ pages/clocklist/normalclock.vue
@@ -0,0 +1,84 @@
+<template>
+	<view>
+		<view class="content">
+			<uni-nav-bar :status-bar="true" backgroundColor="#c4c4e9" :fixed="true" color="white" rightIcon="checkmarkempty" />
+			<gg-nav-group>
+				<gg-cell label="选择闹钟游戏" url="/pages/game/game.vue">
+				</gg-cell>
+			</gg-nav-group>
+			
+			<gg-nav-group>
+				<gg-block>
+					<gg-picker-time label="时间选择" />
+				</gg-block>
+				
+			</gg-nav-group>
+			
+			<gg-nav-group >
+				<gg-block>
+					<gg-checkbox-tag label="闹钟重复" v-model="formData.checkboxTag"  :dataLists="checkLists" :maxNumber="7" />
+				</gg-block>
+			</gg-nav-group>
+			
+			<gg-nav-group>
+				<gg-input-text label="备注"></gg-input-text>
+				<gg-switch labelWidth="140" label="不玩游戏直接关闭闹钟" tip="在闹钟画面有关闭闹钟功能,不完成小游戏,也可以快速关闭闹钟"></gg-switch>
+			</gg-nav-group>
+			
+		</view>
+		
+		
+		
+	</view>
+	
+</template>
+
+<script>
+	// import ggCheckBoxTag from'@10yun/ggui-mobile/lib/gg-checkbox-tag/gg-checkbox-tag.vue'
+	// import ggNavGroup from '@10yun/ggui-mobile/lib/gg-nav-group'
+	// import ggPickerTime from '@10yun/ggui-mobile/lib/gg-picker-time/gg-picker-time.vue'
+	
+	export default {
+		// components: {
+		// 	// ggPickerTime,
+		// 	// ggCheckBoxTag
+		// },
+		data() {
+			return {
+				formData: {
+					checkbox: ['2', '4'],
+					checkboxTag: '2,4',
+				},
+				maxNumber: 7,
+				checkLists: [
+					{ value: '0', text: '一' },
+					{ value: '1', text: '二' },
+					{ value: '2', text: '三' },
+					{ value: '3', text: '四' },
+					{ value: '4', text: '五' },
+					{ value: '5', text: '六' },
+					{ value: '6', text: '七' },
+				],
+			}
+		},
+		methods: {
+			
+		},
+		onLoad: function(option) {
+			console.log(option.id);
+		}
+	}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.gg-block {
+  background-color: #e1e1e1;
+  min-height: 30px;
+}
+</style>
